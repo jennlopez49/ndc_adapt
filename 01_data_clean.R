f055286658a93d20cbb23e8f7459ce28ece414bf
jennlopez49 <jl0049a@student.american.edu>
2023-10-01 23:03:00 -0400

    cleaning other two datasets

--- 01_data_clean.R
+++ 01_data_clean.R
@@ -3,6 +3,8 @@ adapt_general <- read.csv("ndc_content-2/ndc_content.csv")
 adapt_targets <- read.csv("ndc_content-3/ndc_content.csv")
 adapt_vision <- read.csv("ndc_content-4/ndc_content.csv")
 
+
+######### General Adaptation Data ----------------------------------------------
 adapt_gen <- adapt_general
 # changing NAs to actual NAs 
 nas <- c("N/A", "Not Applicable", "Not related", "Not Related", "no new",
@@ -37,12 +39,53 @@ nos <- nos %>% drop_na(ISO)
 
 adapt_gen$Value <- ifelse(adapt_gen$Value == "No", "0", adapt_gen$Value)
 
-# grouping by indicator name
-adapt_gen_nodup <- adapt_gen %>% group_by(Country, Indicator.name) %>% 
+
+
+### TO SWITCH TO WIDE FORM SO EACH COLUMN HAS ONLY INDICATOR FOR EACH COUNTRY
+## switch AFTER finishing hand-coding the last of the string variables
+# # grouping by indicator name
+# adapt_gen_nodup <- adapt_gen %>% group_by(Country, Indicator.name) %>% 
+#   summarise_all(list(~toString(unique(na.omit(.)))))
+
+
+
+
+#########  Adaptation Target Data ----------------------------------------------
+
+## Replacing "Not Available" with NAs
+
+adapt_tar <- adapt_targets
+
+adapt_tar$Value <- ifelse(adapt_tar$Value == "Not Available", NA, 
+                          adapt_tar$Value)
+adapt_tar <- adapt_tar %>% drop_na(Value)
+
+adapt_tar_nodup <- adapt_tar %>% group_by(Country, Indicator.ID) %>%
   summarise_all(list(~toString(unique(na.omit(.)))))
 
+## pulling out financial targets 
 
+financial_targets <- adapt_tar[grepl("USD", adapt_tar$Value),]
 
+# cleaning - getting rid of "spent" costs 
+financial_targets <- financial_targets[!grepl("spent", financial_targets$Value),]
 
+### list of countries 
 
+countries_finan <- unique(financial_targets$Country)
 
+#########  Adaptation Vision Data ----------------------------------------------
+
+adapt_vis <- adapt_vision
+
+# reusing the string matches from earlier
+
+adapt_vis$Value <- ifelse(grepl(paste(nas, collapse = "|"), adapt_vis$Value, 
+                                ignore.case = TRUE),
+                          NA, adapt_vis$Value)
+adapt_vis$Value <- ifelse(grepl(paste(matches, collapse = "|"), adapt_vis$Value,
+                                ignore.case = TRUE), 0, 
+                          adapt_vis$Value)
+adapt_vis$Value <- ifelse(grepl(paste(id, collapse = "|"), adapt_vis$Value,
+                                ignore.case = TRUE), 1, 
+                          adapt_vis$Value)
