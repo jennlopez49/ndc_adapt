07c97283b3e587fd2d715bb6bf4efbd6fadcfd54
jennlopez49 <jl0049a@student.american.edu>
2023-10-23 13:59:25 -0400

    variable selection for financial and non financial needs

--- Lasso_Results.R
+++ Lasso_Results.R
@@ -1,57 +1,69 @@
 ### loading previous vars ---------
-rep_data <- read.csv("~/Dropbox (jl0049a)/2022_joint_Research_Todd_Jennifer_Karl/political_constraints_ambition/updated_mitigation.csv")
-rep_data_cl <- rep_data[,-c(43:196)]
-rep_short <- rep_data_cl[, -c(3:4, 44:70, 74:100)]
+ren_data <- read.csv("~/Dropbox (jl0049a)/2022_joint_Research_Todd_Jennifer_Karl/political_constraints_ambition/renewable-longitudinal-data_0828.csv")
+ren_wide <- ren_data %>% subset(subset = ren_data$year == 2018)
+
+# emissions 
+em_data <- read.csv("~/Dropbox (jl0049a)/2022_joint_Research_Todd_Jennifer_Karl/political_constraints_ambition/updated_mitigation.csv")
+em_short <- ren_data[, c(2, 197,225, 227, 255)]
+
+### ndc data
 full_data <- read.csv("full_data.csv")
 ### merged 
-merged <- merge(rep_short, full_data, by = "country.code")
+merged <- merge(ren_wide, full_data, by = "country.code")
+
+merged_full <- merge(merged, em_short, by = "country.code")
 
 ### Calculating/Creating New vars --------
 ### C02 & GHG Change 
-merged$co2_1990 <- as.numeric(merged$co2_1990)
+merged_full$co2_1990 <- as.numeric(merged_full$co2_1990)
 
-merged$change_co2 <- merged$co2_2018 - merged$co2_1990
-merged$change_ghg <- merged$ghg2019 - merged$ghg1990
+merged_full$change_co2 <- merged_full$co2_2018 - merged_full$co2_1990
+merged_full$change_ghg <- merged_full$ghg2018 - merged_full$ghg1990
 
 ### Adding Binary Vars 
 # financial
-merged$finan_needs_imp <- ifelse(merged$Financial.needs.for.implementation > 0, 1, 0)
-merged$Other_needs <- ifelse(merged$Other.non.financial.support.needs > 0, 1, 0)
+merged_full$finan_needs_imp <- ifelse(merged_full$Financial.needs.for.implementation > 0, 1, 0)
+merged_full$Other_needs <- ifelse(merged_full$Other.non.financial.support.needs > 0, 1, 0)
 # vulnerability & costs 
-merged$costs <- ifelse(merged$Current.economic.loss.and.damage.figures > 0, 1, 0)
-merged$future_costs <- ifelse(merged$Future.economic.loss.and.damage.figures > 0, 1, 0)
+merged_full$costs <- ifelse(merged_full$Current.economic.loss.and.damage.figures > 0, 1, 0)
+merged_full$future_costs <- ifelse(merged_full$Future.economic.loss.and.damage.figures > 0, 1, 0)
 
-merged$loss_damage <- ifelse(merged$Loss.and.damage.mentioned > 0, 1, 0)
-merged$non_econ_loss <- ifelse(merged$Non.economic.loss.and.damage > 0, 1, 0)
+merged_full$loss_damage <- ifelse(merged_full$Loss.and.damage.mentioned > 0, 1, 0)
+merged_full$non_econ_loss <- ifelse(merged_full$Non.economic.loss.and.damage > 0, 1, 0)
 
 # Migration
-merged$migration <- ifelse(merged$Human.mobility > 0, 1, 0)
+merged_full$migration <- ifelse(merged_full$Human.mobility > 0, 1, 0)
+
+### Checking for NAs -- looks like Taiwan, Venezula, Yemen, South Sudan
+countries <- merged_full %>% group_by(country.name) %>% 
+  summarise(across(everything(), ~ sum(is.na(.))))
+
+### Checking by Category --- fuel_pct, prev.leader, reelect, turnover_a, vulnerable group vars 
+merged_full %>% 
+  summarise(across(everything(), ~ sum(is.na(.))))
 
 
-## Getting rid of certain vars 
-merged_clean <- merged[, -c(8:13)]
-merged_clean <- subset(merged_clean, select = -c(X2nd_party, X3rd_party,
-                                  top_party, Country.y, ambition_dv))
 ### Variable Selection -----
-reg_data <- merged_clean[, -c(1:2)]
-reg_data <- subset(reg_data, select = -c(coal_elec_2015, oil_elec_2015,
-                                         renew_2015, hydro_2015, green_party_votes.y,
-                                         green_party_seats.y,
-                                         eff_num_parties_13,eff_num_parties_14,
-                                         eff_num_parties_15, eff_num_parties_16,
-                                         eff_num_parties_17, eff_num_parties_18,
-                                         eco.party.man, env.party.man, 
-                                         green_party_seats.x, const_hosterml,
-                                         WJP_Rule_of_Law_19, green_party_votes.x,
-                                         wjp_rule_18,govt_spend_18, 
-                                         govt_spend_19, green_party_dummy.x
-                                         ))
-reg_full <- lm(Financial.needs.for.implementation ~ .  -Other.non.financial.support.needs
-                - Finan_Needs 
-               - finan_needs_imp - Other_needs - party_pos_env
-               , 
-               data = reg_data)
-reg_null <- lm(Financial.needs.for.implementation ~ 1, data = merged_clean)
+reg_data_countries <- merged_full %>% filter(!(country.name %in% c("Taiwan", 
+                                                                   "Venezuela", "Yemen",
+                                                                   "South Sudan")))
+
+reg_data <- reg_data_countries %>% select(!c(fuel.pct, prev.leader, reelect, 
+                                             turnover_a,
+                                             Identification.of.children.as.a.vulnerable.group,
+                                             Identification.of.young.people.as.a.vulnerable.group,
+                                             Country.x,
+                                             Country.y,
+                                             Other.non.financial.support.needs,
+                                             Finan_Needs,
+                                             finan_needs_imp,
+                                             Other_needs,
+                                             country.code,
+                                             country.name))
+reg_data_no_nas <- na.omit(reg_data)
+reg_full <- lm(Financial.needs.for.implementation ~ ., 
+               data = reg_data_no_nas)
+reg_null <- lm(Financial.needs.for.implementation ~ 1, data = reg_data_no_nas)
 
 step_out <- step(reg_null, 
                  scope = list(lower = reg_null, upper = reg_full),
@@ -65,3 +77,55 @@ cor_data_new <- cor_data_sub %>% na.omit()
 correlations <- cor(cor_data_new)
 write.csv(correlations, "correlations_climatewatch.csv")
 ### Lasso 
+
+
+reg <- lm(Financial.needs.for.implementation ~ ., data = reg_data_no_nas) 
+x <- model.matrix(reg)
+dim(x)
+y <- reg_data_no_nas$Financial.needs.for.implementation
+
+set.seed(123)
+lr_cv <- cv.glmnet(x, y)
+
+plot(lr_cv)
+
+coef(lr_cv)
+
+
+### Non-financial 
+
+reg_data_nonfin <- reg_data_countries %>% select(!c(fuel.pct, prev.leader, reelect, 
+                                             turnover_a,
+                                             Identification.of.children.as.a.vulnerable.group,
+                                             Identification.of.young.people.as.a.vulnerable.group,
+                                             Country.x,
+                                             Country.y,
+                                             Financial.needs.for.implementation,
+                                             Finan_Needs,
+                                             finan_needs_imp,
+                                             Other_needs,
+                                             country.code,
+                                             country.name))
+reg_non_cl <- na.omit(reg_data_nonfin)
+reg_full_non <- lm( Other.non.financial.support.needs~ ., 
+               data = reg_non_cl)
+reg_null_non <- lm(Other.non.financial.support.needs ~ 1, data = reg_non_cl)
+
+step_out_non <- step(reg_null, 
+                 scope = list(lower = reg_null, upper = reg_full),
+                 method = "forward")
+summary(step_out_non)
+
+
+
+reg <- lm(Other.non.financial.support.needs ~ ., data = reg_non_cl) 
+x <- model.matrix(reg)
+dim(x)
+y <- reg_non_cl$Other.non.financial.support.needs
+
+set.seed(123)
+lr_cv <- cv.glmnet(x, y)
+
+plot(lr_cv)
+
+coef(lr_cv)
