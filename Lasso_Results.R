26e6adad8fb4b6e69a2dbc8a6db795abc6337240
jennlopez49 <jl0049a@student.american.edu>
2023-10-30 14:56:58 -0400

    only 2021 subs

--- Lasso_Results.R
+++ Lasso_Results.R
@@ -1,56 +1,75 @@
 ### loading previous vars ---------
 ren_data <- read.csv("~/Dropbox (jl0049a)/2022_joint_Research_Todd_Jennifer_Karl/political_constraints_ambition/renewable-longitudinal-data_0828.csv")
-ren_wide <- ren_data %>% subset(subset = ren_data$year == 2018)
+ren_wide <- ren_data %>% subset(subset = ren_data$year == 2021)
 
-# emissions 
-em_data <- read.csv("~/Dropbox (jl0049a)/2022_joint_Research_Todd_Jennifer_Karl/political_constraints_ambition/updated_mitigation.csv")
-em_short <- ren_data[, c(2, 197,225, 227, 255)]
+# emissions -- stops in 2018 
+# em_data <- read.csv("~/Dropbox (jl0049a)/2022_joint_Research_Todd_Jennifer_Karl/political_constraints_ambition/updated_mitigation.csv")
+# em_short <- ren_data[, c(2, 197,225, 227, 255)]
 
 ### ndc data
 full_data <- read.csv("full_data.csv")
+
+## submissions years
+sub_years <- readxl::read_excel("years_submissions.xlsx")
+sub_years$submission_year <- ifelse(sub_years$submission_year == "NA", NA,
+                                    sub_years$submission_year)
 ### merged 
 merged <- merge(ren_wide, full_data, by = "country.code")
 
-merged_full <- merge(merged, em_short, by = "country.code")
+merged_full <- merge(merged, sub_years, by = "country.code")
+
+### FILTERING TO ONLY 2021 
+
+merged_2021 <- merged_full %>% filter(submission_year == "2021")
 
 ### Calculating/Creating New vars --------
-### C02 & GHG Change 
-merged_full$co2_1990 <- as.numeric(merged_full$co2_1990)
-
-merged_full$change_co2 <- merged_full$co2_2018 - merged_full$co2_1990
-merged_full$change_ghg <- merged_full$ghg2018 - merged_full$ghg1990
+# ### C02 & GHG Change 
+# merged_full$co2_1990 <- as.numeric(merged_full$co2_1990)
+# 
+# merged_full$change_co2 <- merged_full$co2_2018 - merged_full$co2_1990
+# merged_full$change_ghg <- merged_full$ghg2018 - merged_full$ghg1990
 
 ### Adding Binary Vars 
 # financial
-merged_full$finan_needs_imp <- ifelse(merged_full$Financial.needs.for.implementation > 0, 1, 0)
-merged_full$Other_needs <- ifelse(merged_full$Other.non.financial.support.needs > 0, 1, 0)
+merged_2021$finan_needs_imp <- ifelse(merged_2021$Financial.needs.for.implementation > 0, 1, 0)
+merged_2021$Other_needs <- ifelse(merged_2021$Other.non.financial.support.needs > 0, 1, 0)
 # vulnerability & costs 
-merged_full$costs <- ifelse(merged_full$Current.economic.loss.and.damage.figures > 0, 1, 0)
-merged_full$future_costs <- ifelse(merged_full$Future.economic.loss.and.damage.figures > 0, 1, 0)
+merged_2021$costs <- ifelse(merged_2021$Current.economic.loss.and.damage.figures > 0, 1, 0)
+merged_2021$future_costs <- ifelse(merged_2021$Future.economic.loss.and.damage.figures > 0, 1, 0)
 
-merged_full$loss_damage <- ifelse(merged_full$Loss.and.damage.mentioned > 0, 1, 0)
-merged_full$non_econ_loss <- ifelse(merged_full$Non.economic.loss.and.damage > 0, 1, 0)
+merged_2021$loss_damage <- ifelse(merged_2021$Loss.and.damage.mentioned > 0, 1, 0)
+merged_2021$non_econ_loss <- ifelse(merged_2021$Non.economic.loss.and.damage > 0, 1, 0)
 
 # Migration
-merged_full$migration <- ifelse(merged_full$Human.mobility > 0, 1, 0)
+merged_2021$migration <- ifelse(merged_2021$Human.mobility > 0, 1, 0)
 
 ### Checking for NAs -- looks like Taiwan, Venezula, Yemen, South Sudan
-countries <- merged_full %>% group_by(country.name) %>% 
+countries <- merged_2021 %>% group_by(country.name) %>% 
   summarise(across(everything(), ~ sum(is.na(.))))
 
 ### Checking by Category --- fuel_pct, prev.leader, reelect, turnover_a, vulnerable group vars 
-merged_full %>% 
+merged_2021 %>% 
   summarise(across(everything(), ~ sum(is.na(.))))
 
 ### saving 
 
 write.csv(merged_full, "merged_full.csv")
 ### Variable Selection -----
-reg_data_countries <- merged_full %>% filter(!(country.name %in% c("Taiwan", 
+reg_data_countries <- merged_2021 %>% filter(!(country.name %in% c("Taiwan", 
                                                                    "Venezuela", "Yemen",
                                                                    "South Sudan")))
 
 reg_data <- reg_data_countries %>% select(!c(fuel.pct, prev.leader, reelect, 
+                                             turnover,
+                                             turnover90,
+                                             turnover00,
+                                             years.in.office,
+                                             parliamentary,
+                                             finite.term,
+                                             term.limit,
+                                             explicit.subsidy,
+                                             implicit.subsidy,
+                                             green_party,
                                              turnover_a,
                                              Identification.of.children.as.a.vulnerable.group,
                                              Identification.of.young.people.as.a.vulnerable.group,
@@ -58,8 +77,9 @@ reg_data <- reg_data_countries %>% select(!c(fuel.pct, prev.leader, reelect,
                                              Country.y,
                                              Other.non.financial.support.needs,
                                              Financial.needs.for.implementation,
+                                             renewable.generation,
+                                             renewable.consumption,
                                              Finan_Needs,
-                                             Other_needs,
                                              country.code,
                                              country.name))
 reg_data_no_nas <- na.omit(reg_data)
@@ -97,6 +117,16 @@ coef(lr_cv)
 ### Non-financial 
 
 reg_data_nonfin <- reg_data_countries %>% select(!c(fuel.pct, prev.leader, reelect, 
+                                                    turnover,
+                                                    turnover90,
+                                                    turnover00,
+                                                    years.in.office,
+                                                    parliamentary,
+                                                    finite.term,
+                                                    term.limit,
+                                                    explicit.subsidy,
+                                                    implicit.subsidy,
+                                                    green_party,
                                              turnover_a,
                                              Identification.of.children.as.a.vulnerable.group,
                                              Identification.of.young.people.as.a.vulnerable.group,
