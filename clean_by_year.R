2e0394eafe59208f533497b29a4254fe9560d5c8
jennlopez49 <jl0049a@student.american.edu>
2023-11-01 23:24:13 -0400

    adding table by year

---
+++ clean_by_year.R
@@ -0,0 +1,19 @@
+#### dates of submissions 
+dates <- read.csv("ndc_content.csv")
+dates$country.code <- dates$ISO
+
+## merging 
+full_data_dates <- merge(full_data, dates, by = "country.code")
+full_data_dates$date <- full_data_dates$Value
+
+
+# converting date to just year 
+
+full_data_dates <- full_data_dates %>% mutate(
+  date = as.Date(date, "%m/%d/%Y"),
+  year = year(date)
+)
+
+
+### by year 
+table(full_data_dates$year)
